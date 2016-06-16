3221b74072d01ba6feea0ceece7bdb512013db72
Sam Symons <sam@samsymons.com>
2016-06-15 21:36:26 -0700

    Initial Commit

---
+++ Payments/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Payments
+//
+//  Created by Symons, Sam(AWF) on 15/06/16.
+//  Copyright © 2016 Sam Symons. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
